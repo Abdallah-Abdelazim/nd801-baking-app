0ea0f848e9621cbe287c8cf9611d231e45cc1456
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2018-07-23 06:07:38 +0200

    Finish playing videos

--- app/src/main/java/com/abdallah/bakingapp/fragments/RecipeStepDetailsFragment.java
+++ app/src/main/java/com/abdallah/bakingapp/fragments/RecipeStepDetailsFragment.java
@@ -1,6 +1,10 @@
 package com.abdallah.bakingapp.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +15,19 @@ import com.abdallah.bakingapp.R;
 import com.abdallah.bakingapp.activities.RecipeDetailsActivity;
 import com.abdallah.bakingapp.activities.RecipeStepDetailsActivity;
 import com.abdallah.bakingapp.models.recipe.Step;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import org.parceler.Parcels;
 
@@ -31,10 +48,13 @@ public class RecipeStepDetailsFragment extends Fragment {
      */
     public static final String ARG_STEP = "item_id";
 
+    @BindView(R.id.player_view) PlayerView playerView;
     @BindView(R.id.tv_step_description) TextView stepDescriptionTextView;
     private Unbinder unbinder;
 
     private Step step;
+
+    private SimpleExoPlayer player;
 
 
     /**
@@ -73,15 +93,69 @@ public class RecipeStepDetailsFragment extends Fragment {
                 , false);
         unbinder = ButterKnife.bind(this, fragmentView);
 
+        if (!step.hasVideo()) {
+            playerView.setVisibility(View.GONE);
+        }
+
         stepDescriptionTextView.setText(step.getDescription());
-        // TODO video player
 
         return fragmentView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (step.hasVideo()) {
+            initializePlayer(Uri.parse(step.getVideoUrl()));
+        }
+    }
+
+    /**
+     * Initializes ExoPlayer;
+     * @param mediaUri the URI of the video/audio to play.
+     */
+    private void initializePlayer(Uri mediaUri) {
+        if (player == null) {
+            // Create a default TrackSelector
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            TrackSelection.Factory videoTrackSelectionFactory =
+                    new AdaptiveTrackSelection.Factory(bandwidthMeter);
+            DefaultTrackSelector trackSelector =
+                    new DefaultTrackSelector(videoTrackSelectionFactory);
+
+            // Create the player
+            player = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector);
+
+            // Bind the player to the view.
+            playerView.setPlayer(player);
+
+            // Produces DataSource instances through which media data is loaded.
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getContext(),
+                    Util.getUserAgent(getContext(), getString(R.string.app_name)));
+            // This is the MediaSource representing the media to be played.
+            MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(mediaUri);
+            // Prepare the player with the source.
+            player.prepare(videoSource);
+        }
+    }
+
+    /**
+     * Release ExoPlayer.
+     */
+    private void releasePlayer() {
+        if (player != null) {
+            player.stop();
+            player.release();
+            player = null;
+        }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+        releasePlayer();
     }
 }
